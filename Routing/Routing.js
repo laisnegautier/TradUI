1acfba65699c597e13bf0e9f0b2e45446a7eb71e
mjouault <mjouault@ensc.fr>
2020-02-18 11:33:31 +0100

    Création de la navigation par onglet et par pages

--- Routing/Routing.js
+++ Routing/Routing.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 import LearnTranslateScreen from "./components/LearnTranslateScreen";
 import LearnWordcontextScreen from "./components/LearnWordcontextScreen";
 import PlayFindLanguageScreen from "./components/PlayFindLanguageScreen";
@@ -7,29 +8,70 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default class Routing extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true };
+const defaultNavigationOptions = {
+  headerStyle: {
+    backgroundColor: "#a700a7"
+  },
+  headerTintColor: "#fff",
+  headerTitleStyle: {
+    fontWeight: "bold"
   }
+};
 
-  //Action une fois que l'objet est construit
-  componentDidMount = () => {
-    //Actions à effectuer
-    this.setState({ isLoading: false });
-  };
+const getTabBarIcon = (navigation, focused, tintColor) => {
+  const { routeName } = navigation.state;
+  let iconName;
+  if (routeName === "Learn") {
+    iconName = `ios-wine`;
+  } else if (routeName === "Play") {
+    iconName = `ios-menu`;
+  }
+  return <Ionicons name={iconName} size={25} color={tintColor} />;
+};
 
+const LearnStack = createStackNavigator(
+  {
+    LearnTranslate: LearnTranslateScreen,
+    LearnWordContext: LearnWordcontextScreen
+  },
+  {
+    defaultNavigationOptions: defaultNavigationOptions
+  }
+);
+
+const PlayStack = createStackNavigator(
+  {
+    PlayFindLanguage: PlayFindLanguageScreen
+  },
+  {
+    defaultNavigationOptions: defaultNavigationOptions
+  }
+);
+
+const TabNavigator = createBottomTabNavigator(
+  {
+    Learn: LearnStack,
+    Play: PlayStack
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, tintColor }) =>
+        getTabBarIcon(navigation, focused, tintColor)
+    }),
+    tabBarOptions: {
+      activeTintColor: "tomato",
+      inactiveTintColor: "gray",
+      labelStyle: {
+        fontSize: 14
+      }
+    }
+  }
+);
+
+const AppContainer = createAppContainer(TabNavigator);
+
+export default class Routing extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Routing</Text>
-      </View>
-    );
+    return <AppContainer />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "green"
-  }
-});
